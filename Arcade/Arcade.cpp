454d152d26e4882abd667cd0a668fbaadf40a5d6
DraghiciStefan1984 <draghicistefan1984@gmail.com>
2019-06-23 14:47:02 +0300

    arcade start

--- Arcade/Arcade.cpp
+++ Arcade/Arcade.cpp
@@ -2,9 +2,41 @@
 //
 
 #include <iostream>
+#include "SDL.h"
 using namespace std;
 
-int main()
+const int SCREEN_WIDTH = 224;
+const int SCREEN_HEIGHT = 288;
+
+int main(int argc, char* argv[])
 {
-    std::cout << "Hello World!\n";
+	if (SDL_Init(SDL_INIT_VIDEO)) cout << "SDL init failed!!!" << endl;
+ 	SDL_Window* window = SDL_CreateWindow("Arcade", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+	
+	if (window == nullptr)
+	{
+		cout << "Cannot create window: " << SDL_GetError() << endl;
+		return EXIT_FAILURE;
+	}
+
+	SDL_Event sdlEvent;
+	bool running = true;
+
+	while (running)
+	{
+		while (SDL_PollEvent(&sdlEvent))
+		{
+			switch (sdlEvent.type)
+			{
+			case SDL_QUIT:
+				running = false;
+				break;
+			}
+		}
+	}
+
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	return EXIT_SUCCESS;
 }
